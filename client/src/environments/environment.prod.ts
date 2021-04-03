e0f4a718c12ce0cc6938b6ee85ab67b265573096
Andrew Bateman <gomezbateman@yahoo.com>
2021-04-03 17:48:36 +0200

    access info. removed

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,9 +1,6 @@
 export const environment = {
   production: true,
-  NASA_KEY: "RS7JGQcH0ZxUx4SQRuTRgidsJiWfdAoki0Ma01OM",
-  NYT_BEST_KEY: "C3M5V8WyUwpntGKyvzhUcQ4edygGbbtt",
-  NYT_NEWS_KEY: "D16QJGKByS5InSdIUWIucgPZ4rUeQFA6",
-  GITHUB_KEY: "42f1488efd3efca8d1efc5788e390dbd10e20d22",
+
   application: {
     name: "API Dashboard",
   },
