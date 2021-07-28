7f9c399212f3fed18995c7747ff34fc69ff91550
AndrewJBateman <gomezbateman@yahoo.com>
2021-07-28 17:45:19 +0200

    fix: env api keys set up

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,7 +1,10 @@
 export const environment = {
   production: false,
   // add NASA, NYT news, NYT bestsellers & Github keys here
-
+  NASA_KEY: "NASA_KEY",
+  NYT_BEST_KEY: "NYT_BEST_KEY",
+  NYT_NEWS_KEY: "NYT_NEWS_KEY",
+  GITHUB_KEY: "GITHUB_KEY",
   application: {
     name: "API Dashboard",
   },
