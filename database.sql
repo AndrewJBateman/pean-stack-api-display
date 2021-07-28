cf95d04756800613ed5795ed50502c2bc89a1a31
AndrewJBateman <gomezbateman@yahoo.com>
2021-07-28 16:55:36 +0200

    chore: add database data to sql file as backup

--- database.sql
+++ database.sql
@@ -29,10 +29,32 @@ CREATE TABLE countryData(
   bordersMed  BOOLEAN
 );
 
+INSERT INTO metalsData(element, density, color, symbol) VALUES ('Magnesium', 1.74, '#696969', 'Mg');
+INSERT INTO metalsData(element, density, color, symbol) VALUES ('Titanium', 4.5, 'blue', 'Ti');
+INSERT INTO metalsData(element, density, color, symbol) VALUES ('Vanadium', 6.11, '#d4b8cf', 'V');
+INSERT INTO metalsData(element, density, color, symbol) VALUES ('Chromium', 7.19, '#fff500', 'Cr');
+INSERT INTO metalsData(element, density, color, symbol) VALUES ('Iron', 7.87, '#d4661b', 'Fe');
+INSERT INTO metalsData(element, density, color, symbol) VALUES ('Cobalt', 8.9, '#0047AB', 'Co');
+INSERT INTO metalsData(element, density, color, symbol) VALUES ('Copper', 8.94, '#b87333', 'Cu');
 INSERT INTO metalsData(element, density, color, symbol) VALUES ('Silver', 10.49, 'silver', 'Ag');
+INSERT INTO metalsData(element, density, color, symbol) VALUES ('Gold', 19.3, 'gold', 'Au');
+INSERT INTO metalsData(element, density, color, symbol) VALUES ('Platinum', 21.45, '#e5e4e2', 'Pt');
+
 INSERT INTO companyPerfData(year, sales, expenses) VALUES ('2008', 1020, 570);
+INSERT INTO companyPerfData(year, sales, expenses) VALUES ('2009', 1060, 700);
+INSERT INTO companyPerfData(year, sales, expenses) VALUES ('2010', 1020, 550);
+INSERT INTO companyPerfData(year, sales, expenses) VALUES ('2011', 1250, 800);
+INSERT INTO companyPerfData(year, sales, expenses) VALUES ('2012', 1090, 900);
+INSERT INTO companyPerfData(year, sales, expenses) VALUES ('2013', 900, 800);
+
 INSERT INTO frameworkMarketData(framework, share) VALUES ('React', 57.5);
+INSERT INTO frameworkMarketData(framework, share) VALUES ('Angular', 31.5);
+INSERT INTO frameworkMarketData(framework, share) VALUES ('Vue', 11.0);
+
 INSERT INTO gaugeData(label, reading) VALUES ('Memory', 80);
+INSERT INTO gaugeData(label, reading) VALUES ('CPU', 55);
+INSERT INTO gaugeData(label, reading) VALUES ('Network', 68);
+
 INSERT INTO countryData(name, population, bordersMed) VALUES ('Russia', 146, FALSE);
 INSERT INTO countryData(name, population, bordersMed) VALUES ('Germany', 84, FALSE);
 INSERT INTO countryData(name, population, bordersMed) VALUES ('Turkey', 85, TRUE);
