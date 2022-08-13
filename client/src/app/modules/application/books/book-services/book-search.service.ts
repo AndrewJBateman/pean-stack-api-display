762679a82c1a9cc0ed4f60d2cbf0adf5254dc534
AndrewJBateman <gomezbateman@yahoo.com>
2022-08-13 21:25:01 +0200

    fix: filter books without images

---
+++ client/src/app/modules/application/books/book-services/book-search.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { catchError, map, tap, filter } from "rxjs/operators";
+
+import { Book, GoogleBooksApiInterface } from "../book-models/books";
+
+const apiUrl = "https://www.googleapis.com/books/v1/volumes";
+
+@Injectable({
+  providedIn: "root",
+})
+export class GoogleBookService {
+  constructor(private http: HttpClient) {}
+
+  findBook(title: string): Observable<Book[]> {
+    return this.http
+      .get<GoogleBooksApiInterface>(`${apiUrl}?q=${title}&maxResults=40`)
+      .pipe(
+        tap((data: GoogleBooksApiInterface) =>
+          console.log("data: ", data.items)
+        ),
+        map((data: GoogleBooksApiInterface) => data.items.filter(item => item.saleInfo.saleability !== "NOT_FOR_SALE") || []),
+        catchError((err) => {
+          throw "error in getting API data. Details: " + err;
+        })
+      );
+  }
+}
